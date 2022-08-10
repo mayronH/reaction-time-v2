f5cdd0136dd69ad3f92691e1930ce11b8121f776
mayronH <38799771+mayronH@users.noreply.github.com>
2022-08-10 11:38:48 -0300

    result reaction component mount test

--- src/test/basic-components.test.ts
+++ src/test/basic-components.test.ts
@@ -9,6 +9,7 @@ import FooterComponent from '../components/FooterComponent.vue'
 import ChartLine from '../components/ChartLine.vue'
 import ChartBar from '../components/ChartBar.vue'
 import LoadingComponent from '../components/LoadingComponent.vue'
+import ResultReaction from '../components/ResultReaction.vue'
 
 test('NavigationBar', async () => {
   expect(NavigationBar).toBeTruthy()
@@ -80,5 +81,18 @@ test('mount LoadingComponent', async () => {
 
   const wrapper = mount(LoadingComponent)
 
+  expect(wrapper.html()).toMatchSnapshot()
+})
+
+test('mount ResultReaction', async () => {
+  expect(ResultReaction).toBeTruthy()
+
+  const wrapper = mount(ResultReaction, {
+    props: {
+      score: 200,
+    },
+  })
+
+  expect(wrapper.text()).toContain('200 ms')
   expect(wrapper.html()).toMatchSnapshot()
 })
